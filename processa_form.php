fc93b934fcb979ca96c422058c200c178f2b2b49
stefanopaesano <stefanopaesano022@gmail.com>
2024-02-07 17:27:58 +0100

    esercizio  con milestone 1 e 2

---
+++ processa_form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+</head>
+<body>
+
+
+<?php
+    
+
+    if(isset($_GET['genera_password'])) {
+        
+        if(isset($_GET['lunghezza_password'])) {
+            $lunghezza_password = $_GET['lunghezza_password'];
+            
+            $password_generata = randomPassword($lunghezza_password);
+           
+            echo "<p>Password generata: $password_generata</p>";
+        } 
+    }
+
+    // Funzione per generare una password casuale stackoverflow
+    function randomPassword($lunghezza) {
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        $pass = "";
+        for ($i = 0; $i < $lunghezza; $i++) {
+            $n = rand(0, strlen($alphabet)-1);
+            $pass .= $alphabet[$n];
+        }
+        return $pass;
+    }
+?>
+
+
+    
+</body>
+</html>
